7d1f91d5bb8c7f64cce5f69fe3fee3145fc03997
Samruddhi Santosh Kulkarni <sk5835527@gmail.com>
2026-05-23 19:56:15 +0530

    Add files via upload

---
+++ Addition.py
@@ -0,0 +1,20 @@
+# Program to perforrm addition of two integers
+
+def main():
+
+    iValue1 = 0
+    iValue2 = 0
+    iRet = 0
+
+    print("Enter first number:")
+    iValue1 = int(input())
+
+    print("Enter second number:")
+    iValue2 = int(input())
+
+    iRet = iValue1 + iValue2;
+
+    print("Addition is:",iRet)
+
+if __name__ == "__main__":
+    main()
